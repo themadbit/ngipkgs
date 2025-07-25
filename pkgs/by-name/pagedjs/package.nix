4a6aa8c9315e58116f6e037838c0a0e781322f11
themadbit <marktanui75@gmail.com>
2025-07-25 14:45:06 +0300

    init pagejs

--- pkgs/by-name/pagedjs/package.nix
+++ pkgs/by-name/pagedjs/package.nix
@@ -5,16 +5,18 @@
   buildNpmPackage,
   nix-update-script,
   nodejs_22,
+  makeWrapper,
+  chromium,
 }:
 
 buildNpmPackage (finalAttrs: {
-  pname = "pagedjs-cli";
-  version = "0.4.3-unstable-2024-05-31";
+  pname = "pagedjs";
+  version = "0.5.0-unstable-2024-10-04";
 
   src = fetchFromGitHub {
     owner = "pagedjs";
-    repo = "pagedjs-cli";
-    rev = "d682e19ee5d14bfe07ad1726540e2423ede75a05";
+    repo = "pagedjs";
+    rev = "c71f8ab56905168abafa31b7c2329d7f9c1b1dc8";
     hash = "sha256-7DXfBMi6OPNUT1XM5Gtsbk8xK4rz5xmDbJAPulrVTmE=";
   };
 
@@ -29,17 +31,24 @@ buildNpmPackage (finalAttrs: {
     "--ignore-scripts"
   ];
 
+  dontNpmBuild = true;
+
+  # postInstall = ''
+  #   wrapProgram $out/bin/puppeteer \
+  #     --set PUPPETEER_EXECUTABLE_PATH ${chromium}/bin/chromium
+  # '';
+
   nativeBuildInputs = [
     nodejs_22
   ];
 
   meta = {
-    description = "Command line interface for Pagedjs";
+    description = "JavaScript library for paged media in the browser";
     homepage = "https://pagedjs.org";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ themadbit ];
     teams = [ lib.teams.ngi ];
     platforms = lib.platforms.all;
-    mainProgram = "pagedjs-cli";
+    mainProgram = "pagedjs";
   };
 })
