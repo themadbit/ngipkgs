20bf741742624085f0239cc8ff3fe93a12fa4724
themadbit <marktanui75@gmail.com>
2025-08-28 13:09:38 +0300

    reoxide: init at 0.6.1-unstable-2025-08-27
    Signed-off-by: themadbit <marktanui75@gmail.com>

---
+++ projects/ReOxide/services/reoxided/tests/basic.nix
@@ -0,0 +1,25 @@
+{
+  sources,
+  ...
+}:
+{
+  name = "reoxide";
+
+  nodes = {
+    machine =
+      { ... }:
+      {
+        imports = [
+          sources.modules.ngipkgs
+          sources.modules.services.reoxided
+          sources.examples.ReOxide."Enable reoxided"
+        ];
+      };
+  };
+
+  testScript =
+    { nodes, ... }:
+    ''
+      start_all()
+    '';
+}
