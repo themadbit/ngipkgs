8e842ce704a2392e564e87bb205ffa6c7ed31630
themadbit <marktanui75@gmail.com>
2025-09-12 11:15:14 +0300

    add plugin: reoxide-plugin-simple
    Signed-off-by: themadbit <marktanui75@gmail.com>

---
+++ pkgs/by-name/reoxide-plugin-simple/package.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  fetchFromGitea,
+  clangStdenv,
+
+  meson,
+  clang,
+  ninja,
+  reoxide,
+
+  libllvm,
+  libclang,
+}:
+
+clangStdenv.mkDerivation (finalAttrs: {
+  pname = "reoxide-plugin-simple";
+  version = "0.6.1-unstable-2025-08-27";
+
+  src = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "ReOxide";
+    repo = "plugin-template";
+    rev = "a8457da854c7ba0908c88c58a251b6398ba410f3";
+    hash = "sha256-y9gxryOp1JdM5ttrIP5lge55UZS5hL5RIqLNRE98XFA=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    clang
+    ninja
+    reoxide
+
+    clangStdenv.cc
+  ];
+
+  buildInputs = [
+    libllvm
+    libclang
+  ];
+
+  env = {
+    CC = "${clangStdenv.cc}/bin/clang";
+    CXX = "${clangStdenv.cc}/bin/clang++";
+  };
+
+  mesonFlags = [
+    "-Db_ndebug=true"
+  ];
+
+  meta = with lib; {
+    description = "Simple plugin template for reoxide";
+    homepage = "https://codeberg.org/ReOxide/reoxide";
+    license = licenses.asl20;
+    maintainers = with maintainers; [
+      themadbit
+      eljamm
+    ];
+    teams = with teams; [ ngi ];
+    platforms = platforms.linux;
+  };
+})
